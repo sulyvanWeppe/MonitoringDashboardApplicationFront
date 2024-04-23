31f6ce3599312f57dda2340dead92d24c05d7ae8
Sulyvan WEPPE <Sulyvan.WEPPE@ing.com>
2024-04-23 14:56:38 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MainDrawer from './components/MainDrawer';
+
+function App() {
+  return (
+    <div className="App">
+      <MainDrawer/>
+    </div>
+  );
+}
+
+export default App;
